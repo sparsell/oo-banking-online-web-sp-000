d650cdb08be1bfd587736cbf5984a34ce8e816c7
Sonja Parsell <hello@sparsell.com>
2020-05-12 16:20:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ require 'pry'
       if valid? && sender.balance >= @amount #rejects a transfer if the sender does not have enough funds
         sender.balance -= @amount
         receiver.balance += @amount#can execute a successful transaction between two accounts
+        self.status = "complete"
       else
         puts "You don't have sufficient funds to transfer."
       end
